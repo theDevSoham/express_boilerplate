50efc7c29909baccee739b215191cfa23ad25209
Soham Das <publicsoham.24@gmail.com>
2023-06-08 00:43:25 +0530

    views added

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const express = require('express')
+const express = require("express");
+const path = require("path");
 const PORT = process.env.PORT || 5500;
 
 const app = express();
+app.use(express.static(path.join(__dirname, "views")));
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "html", "index.html"), (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
 });
 
 app.listen(PORT, () => {
-	console.log(`Example app listening on port ${PORT}!`)
+  console.log(`Example app listening on port ${PORT}!`);
 });
